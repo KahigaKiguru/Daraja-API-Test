1a38283934462f99e4618568b2b21723993e5518
KahigaKiguru <KahigaKiguru@gmail.com>
2020-04-11 08:43:13 +0300

    Upgraded and Refactored lipanampesa class with authenticztion capabilities

--- src/LipaNaMpesa.py
+++ src/LipaNaMpesa.py
@@ -1,26 +1,28 @@
 import requests
+import authorize_app
+import user_credentials
 
-access_token = "Access-Token"
+access_token = authorize_app.access_token
 api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
 
-headers = {"Authorization": "Bearer %s" %access_token}
+header = {"Authorization": "Bearer %s" %access_token}
 
 
 request = {
-            "BusinessShortCode": " ",
-            "Password": " ",
-            "Timestamp": " ",
+            "BusinessShortCode": user_credentials.business_shortCode,
+            "Password": user_credentials.generate_password,
+            "Timestamp": user_credentials.generate_timestamp,
             "TransactionType": "CustomerPayBillOnline",
-            "Amount": " ",
-            "PartyA": " ",
-            "PartyB": " ",
-            "PhoneNumber": " ",
+            "Amount": user_credentials.transaction_amount,
+            "PartyA": user_credentials.party_A,
+            "PartyB": user_credentials.business_shortCode,
+            "PhoneNumber": user_credentials.party_A,
             "CallBackURL": "https://ip_address:port/callback",
-            "AccountReference": " ",
-            "TransactionDesc": " "
+            "AccountReference": user_credentials.account_reference,
+            "TransactionDesc": user_credentials.transaction_description
     }
 
-response = requests.post(api_url, json = request, headers = headers)
+response = requests.post(api_url, json=request, headers=header)
 
 print(response.text)
 
