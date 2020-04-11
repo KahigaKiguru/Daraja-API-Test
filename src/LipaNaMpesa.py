3987efa6c791edfa330b7d284a6287857d143cb3
KahigaKiguru <KahigaKiguru@gmail.com>
2020-04-11 04:46:11 +0300

    test placement for requests pip installs

--- src/LipaNaMpesa.py
+++ src/LipaNaMpesa.py
@@ -1,1 +1,26 @@
-print("Hello, this is a unit test !!")
+import requests
+
+access_token = "Access-Token"
+api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+headers = {"Authorization": "Bearer %s" %access_token}
+
+
+request = {
+            "BusinessShortCode": " ",
+            "Password": " ",
+            "Timestamp": " ",
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": " ",
+            "PartyA": " ",
+            "PartyB": " ",
+            "PhoneNumber": " ",
+            "CallBackURL": "https://ip_address:port/callback",
+            "AccountReference": " ",
+            "TransactionDesc": " "
+    }
+
+response = requests.post(api_url, json = request, headers = headers)
+
+print(response.text)
+
