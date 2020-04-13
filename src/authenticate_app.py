8bc75259df3d2e6946a3524b455ea31c67e15ca4
KahigaKiguru <KahigaKiguru@gmail.com>
2020-04-13 22:30:41 +0300

    refactored the app authentication process

--- src/authenticate_app.py
+++ src/authenticate_app.py
@@ -1,16 +1,21 @@
 import  requests
 from requests.auth import HTTPBasicAuth
+
 import user_credentials
 
 
 #Authenticate Application
 
-consumer_key = user_credentials.consumer_key
-consumer_secret = user_credentials.consumer_secret
+def authenticate_application():
+    consumer_key = user_credentials.consumer_key
+    consumer_secret = user_credentials.consumer_secret
 
 
-auth_request = requests.get(user_credentials.authentication_api_URL, auth=HTTPBasicAuth(user_credentials.consumer_key, user_credentials.consumer_secret))
+    auth_request = requests.get(user_credentials.authentication_api_URL, auth=HTTPBasicAuth(user_credentials.consumer_key, user_credentials.consumer_secret))
 
-authrequest_jsonresponse = auth_request.json()
+    authrequest_jsonresponse = auth_request.json()
 
-access_token = authrequest_jsonresponse['access_token']
+    access_token = authrequest_jsonresponse['access_token']
+
+    return str.format(access_token)
+    
